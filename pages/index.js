71414e04b1ea0b77df6215f3ee19bac370cb56ce
Adil Can <adil_can@hotmail.com>
2022-10-14 11:28:29 -0700

    Initial

---
+++ pages/index.js
@@ -0,0 +1,88 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+/* pages/index.js */
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
+
+export default function Home({ breeds = [] }) {
+  const [breedList, setBreedList] = useState([]);
+
+  useEffect(() => {
+    setBreedList(Object.keys(breeds));
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Amplify Hosting Test</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Amplify Hosting Test App</h1>
+        <br />
+        <select>
+          {breedList.map((breed) => (
+            <option key={breed} value={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const url = "https://dog.ceo/api/breeds/list/all";
+  const res = await fetch(url);
+  const data = await res.json();
+  const breeds = data.message;
+
+  return {
+    props: { breeds },
+  };
+}
